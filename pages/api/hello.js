ee060824a59021f5904857cfd10dfb2327e307ae
Court Schuett <schuettc@amazon.com>
2022-09-25 23:08:53 -0500

    fix: add table

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2';
+const config = {
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const client = new EC2Client(config);
+
+export default async function handler(req, res) {
+  const response = await client.send(new DescribeInstancesCommand());
+  if (response) {
+    res.status(200).json({ data: response });
+  } else {
+    res.status(503).json({ data: 'Error getting instances' });
+  }
 }
