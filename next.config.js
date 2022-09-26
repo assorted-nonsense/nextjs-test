299749f2211d30564b8ac7c3380b866a4d339e20
Court Schuett <schuettc@amazon.com>
2022-09-26 00:39:13 -0500

    fix: update deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const withTM = require('next-transpile-modules')([
-  '@cloudscape-design/components',
-]);
+const withTranspileModules = require('next-transpile-modules');
+const withPlugins = require('next-compose-plugins');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = withPlugins(
+  [withTranspileModules(['@cloudscape-design/components'])],
+  nextConfig,
+);
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@cloudscape-design/global-styles';
 
+if (typeof window === 'undefined') React.useLayoutEffect = () => {};
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
