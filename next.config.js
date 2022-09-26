f57aba00b2ef39635b2d77bd6b87d27cd4e826db
Court Schuett <schuettc@amazon.com>
2022-09-26 00:27:01 -0500

    fix: add app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+// Import these dependencies
 const withTranspileModules = require('next-transpile-modules');
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // ...our app configuration
 };
 
+// Here we will add a plugin to our configuration to transpile the CloudScape components into
+// components that are compatible with our NextJS app
 module.exports = withPlugins(
   [withTranspileModules(['@cloudscape-design/components'])],
   nextConfig,
