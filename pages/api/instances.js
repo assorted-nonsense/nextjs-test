47c9d5e062adb6047e2dbbcaac449281b70b1363
Court Schuett <schuettc@amazon.com>
2022-09-26 15:55:34 -0500

    fix: update env

--- pages/api/instances.js
+++ pages/api/instances.js
@@ -2,8 +2,8 @@ import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2';
 const config = {
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 };
 
