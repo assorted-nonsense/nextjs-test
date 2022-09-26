f646ed46ebd75a032b359f66a734ee8d1ebe0076
Court Schuett <schuettc@amazon.com>
2022-09-26 00:36:37 -0500

    fix: remove plugins

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-// Import these dependencies
-const withTranspileModules = require('next-transpile-modules');
-const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([
+  '@cloudscape-design/components',
+]);
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...our app configuration
+  reactStrictMode: true,
 };
 
-// Here we will add a plugin to our configuration to transpile the CloudScape components into
-// components that are compatible with our NextJS app
-module.exports = withPlugins(
-  [withTranspileModules(['@cloudscape-design/components'])],
-  nextConfig,
-);
+module.exports = withTM(nextConfig);
