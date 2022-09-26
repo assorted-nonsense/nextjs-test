e806fdd0b412709840182e899aa6720454db98d3
Court Schuett <schuettc@amazon.com>
2022-09-26 00:33:16 -0500

    fix: remove explicit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '@cloudscape-design/global-styles';
-import '@cloudscape-design/components/app-layout/visual-refresh/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
