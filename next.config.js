361ca93e34dc2b1e3d009cb00f8bd2edcaeaaaad
Court Schuett <schuettc@amazon.com>
2022-10-02 02:18:04 +0000

    Update next.config.js and index.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-const withTranspileModules = require('next-transpile-modules');
-const withPlugins = require('next-compose-plugins');
+const withTranspileModules = require("next-transpile-modules");
+const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+  },
 };
 
 module.exports = withPlugins(
-  [withTranspileModules(['@cloudscape-design/components'])],
-  nextConfig,
+  [withTranspileModules(["@cloudscape-design/components"])],
+  nextConfig
 );
